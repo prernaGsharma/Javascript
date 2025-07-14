dd8da5577287c3dac81242229872c070640ee9f1
prernaGsharma <154942525+prernaGsharma@users.noreply.github.com>
2025-07-14 16:56:02 +0000

    conversion

---
+++ 01_Basic/conversion.js
@@ -0,0 +1,17 @@
+// let score ="22and";
+// console.log(score);
+// console.log(typeof(score));
+
+// let isvalidnumber = Number(score);
+// console.log(isvalidnumber);
+// console.log(typeof(isvalidnumber));
+
+
+let islogin = 1; // empty type = false
+console.log(islogin);
+
+let booleanlogin = Boolean(islogin);
+console.log(typeof(booleanlogin));
+console.log(booleanlogin);
+
+
---
+++ 01_Basic/datatypes.js
@@ -0,0 +1,6 @@
+//there are two types of datatypes 
+//1. primitive  2. non-primitive
+// primitive-- immutable , value cannot change, value pass , there are 7- string, number, boolean , undefined , null, bigint, symbol
+// primitive--->stack
+// non-primitive-- mutable , reference, object array and function
+//non-primitive---. heap
