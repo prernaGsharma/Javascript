f5e1b5c3a0b53d2be29278a0e00d98057a758054
prernaGsharma <154942525+prernaGsharma@users.noreply.github.com>
2025-07-22 05:57:11 +0000

    stringconcept

---
+++ 01_Basic/string.js
@@ -0,0 +1,9 @@
+let name = "prerna"
+let subject= "javascript"
+
+console.log(`my name is ${name} and the subject name is ${subject}`); 
+
+const changename = new String ('Prerna Sharma');
+console.log(changename[3]);
+console.log(changename.toUpperCase());
+console.log(changename.charAt('4'));
